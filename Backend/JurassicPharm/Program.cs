5d6663af04898324ed0821e07b71f167f090e67f
wfederico97 <wuthrich09@gmail.com>
2024-10-19 19:59:43 -0300

    ADD DI for database

--- Backend/JurassicPharm/Program.cs
+++ Backend/JurassicPharm/Program.cs
@@ -1,9 +1,14 @@
+using JurassicPharm.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<jurassic_pharmContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
