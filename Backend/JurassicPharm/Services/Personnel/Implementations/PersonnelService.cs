e4b03c89dc06f6c22f9c1f0c888d1af91659e92f
wfederico97 <wuthrich09@gmail.com>
2024-10-23 00:35:32 -0300

    ADD service

---
+++ Backend/JurassicPharm/Services/Personnel/Implementations/PersonnelService.cs
@@ -0,0 +1,48 @@
+﻿using JurassicPharm.Models;
+using JurassicPharm.Models.DTOs.Personnel;
+using JurassicPharm.Repositories.Personnel.Implementations;
+using JurassicPharm.Repositories.Personnel.Interfaces;
+using JurassicPharm.Services.Personnel.Interfaces;
+
+namespace JurassicPharm.Services.Personnel.Implementations
+{
+    public class PersonnelService : IPersonnelService
+    {
+        private readonly IPersonnelRepository _repository;
+
+        public PersonnelService(IPersonnelRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<PersonnelDTO> GetAllPersonnel()
+        {
+            List<Empleado> personnel = _repository.GetAllPersonnel();
+            return personnel.Select(p => new PersonnelDTO
+            {
+                Apellido = p.Apellido,
+                Nombre = p.Nombre,
+                LegajoEmpleado = p.LegajoEmpleado,
+                CorreoElectronico = p.CorreoElectronico,
+                IdSucursal = (Int32)p.IdSucursal
+
+            }).ToList();
+            
+        }
+
+        public Empleado GetPersonnel(int codigo)
+        {
+            return _repository.GetPersonnel(codigo);
+        }
+
+        public void UpdatePersonnel(Empleado empleado)
+        {
+            _repository.UpdatePersonnel(empleado);
+        }
+
+        public void DeletePersonnel(Empleado empleado)
+        {
+            _repository.DeletePersonnel(empleado);
+        }
+    }
+}
---
+++ Backend/JurassicPharm/Services/Personnel/Interfaces/IPersonnelService.cs
@@ -0,0 +1,13 @@
+﻿using JurassicPharm.Models;
+using JurassicPharm.Models.DTOs.Personnel;
+
+namespace JurassicPharm.Services.Personnel.Interfaces
+{
+    public interface IPersonnelService
+    {
+        List<PersonnelDTO> GetAllPersonnel();
+        Empleado GetPersonnel(int codigo);
+        void UpdatePersonnel(Empleado empleado);
+        void DeletePersonnel(Empleado empleado);
+    }
+}
