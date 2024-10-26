ae32b9a8a0ed705ae6640557edfc4ab776690682
julian-rinaudo <julianrinaudo18@gmail.com>
2024-10-26 16:35:16 -0300

    Add invoices table

---
+++ Frontend/Javascript/Facturas/index.js
@@ -0,0 +1,13 @@
+addEventListener("load", async (event) => {
+    await generateTable();
+});
+
+const generateTable = async () => {
+    const invoices = await getInvoices();
+}
+
+const getInvoices = () => {
+    //TODO: FETCH REAL DATA FROM BACKEND
+   
+    return 
+}
