d0238a9fa5144fc4015191541883eb2f07784aaa
Federico Wuthrich <wuthrich09@gmail.com>
2024-11-01 10:45:57 -0300

    Nuevos DTO para manejo de API de sucursales y Ciudades

--- Backend/JurassicPharm/DTO/Personnel/GetPersonnelDTO.cs
+++ Backend/JurassicPharm/DTO/Personnel/GetPersonnelDTO.cs
@@ -5,7 +5,9 @@
         public int LegajoEmpleado { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public int IdSucursal { get; set; }
         public string CorreoElectronico { get; set; }
+        public string Rol { get; set; }
+        public string Ciudad { get; set; }
+        public string Sucursal { get; set; }
     }
 }
---
+++ Backend/JurassicPharm/DTO/Stores/GetStoreDTO.cs
@@ -0,0 +1,12 @@
+﻿using JurassicPharm.DTO.Personnel;
+
+namespace JurassicPharm.DTO.Stores
+{
+    public class GetStoreDTO
+    {
+        public int IdSucursal { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+        public List<GetPersonnelSummaryDTO> Empleados { get; set; }
+    }
+}
