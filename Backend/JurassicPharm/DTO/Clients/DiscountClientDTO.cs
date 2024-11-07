9bf733bd9a2b0c3e3913817e193a024d4b99f442
Federico Wuthrich <wuthrich09@gmail.com>
2024-11-07 00:42:58 -0300

    FW - BK - Clients DTOs

---
+++ Backend/JurassicPharm/DTO/Clients/DiscountClientDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace JurassicPharm.DTO.Clients
+{
+    public class DiscountClientDTO
+    {
+
+            public string ClientName { get; set; }
+            public string ClientSurname { get; set; }
+            public string SocialSecurityName { get; set; }
+            public string Location { get; set; }
+            public int MaxDiscount { get; set; }
+    }
+}
