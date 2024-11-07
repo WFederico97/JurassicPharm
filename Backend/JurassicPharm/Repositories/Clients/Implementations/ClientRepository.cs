5d80a640a4dccd3debff0ffb5da67eee0c67c6b5
Federico Wuthrich <wuthrich09@gmail.com>
2024-11-07 00:46:35 -0300

    FW - BK - Supplies and Clients entities

---
+++ Backend/JurassicPharm/Repositories/Clients/Implementations/ClientRepository.cs
@@ -0,0 +1,141 @@
+﻿using JurassicPharm.DTO.Clients;
+using JurassicPharm.Models;
+using JurassicPharm.Repositories.Clients.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace JurassicPharm.Repositories.Clients.Implementations
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly JurassicPharmContext _context;
+        public ClientRepository(JurassicPharmContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CreateClient(CreateClientDTO client)
+        {
+            try
+            {
+                var HealthPlan = await _context.ObrasSociales.Where(healthPlan => healthPlan.IdObraSocial == client.IdHealthPlan).ToListAsync();
+                if (HealthPlan == null)
+                {
+                    throw new Exception("Obra social no existe");
+                }
+                var City = await _context.Ciudades.Where(city => city.IdCiudad == client.IdCity).ToListAsync();
+                if (City == null)
+                {
+                    throw new Exception("Ciudad no existe");
+                }
+                Cliente cliente = new Cliente()
+                {
+                    IdObraSocial = client.IdHealthPlan,
+                    IdCiudad = client.IdCity,
+                    Nombre = client.Name,
+                    Apellido = client.Lastname,
+                    CorreoElectronico = client.Email,
+                    Calle = client.Street,
+                    Altura = client.Number
+                };
+                await _context.Clientes.AddAsync(cliente);
+                if (await _context.SaveChangesAsync() == 1)
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+        public async Task<Cliente?> GetClienteById(int idCliente)
+        {
+            return await _context.Clientes.FirstOrDefaultAsync(cliente => cliente.IdCliente == idCliente);
+        }
+
+        public async Task<bool> DeleteClient(int idCliente)
+        {
+            Cliente clientToDelete = await _context.Clientes.Where(cliente => cliente.IdCliente == idCliente).FirstOrDefaultAsync();
+            if (clientToDelete == null)
+            {
+                throw new Exception("Client Not Found");
+            }
+            _context.Clientes.Remove(clientToDelete);
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateClient(UpdateClientDTO client, int idCliente)
+        {
+            Cliente clientToUpdate = await _context.Clientes.Where(client => client.IdCliente == idCliente).FirstOrDefaultAsync();
+            if(clientToUpdate == null)
+            {
+                throw new Exception("Client Not Found");
+            }
+            if(!string.IsNullOrEmpty(client.Nombre) && client.Nombre != clientToUpdate.Nombre)
+            {
+                clientToUpdate.Nombre = client.Nombre;
+            }
+            if (!string.IsNullOrEmpty(client.Apellido) && client.Apellido!= clientToUpdate.Apellido)
+            {
+                clientToUpdate.Apellido = client.Apellido;
+            }
+            if (!string.IsNullOrEmpty(client.Calle) && client.Calle != clientToUpdate.Calle)
+            {
+                clientToUpdate.Calle = client.Calle;
+            }
+            if (!string.IsNullOrEmpty(client.CorreoElectronico) && client.CorreoElectronico != clientToUpdate.CorreoElectronico)
+            {
+                clientToUpdate.CorreoElectronico= client.CorreoElectronico;
+            }
+            if (client.Altura.HasValue && client.Altura != clientToUpdate.Altura)
+            { 
+            clientToUpdate.Altura = client.Altura;
+            }
+            if(client.IdCiudad.HasValue && client.IdCiudad != clientToUpdate.IdCiudad)
+            {
+                clientToUpdate.IdCiudad = client.IdCiudad;
+            }
+            if(client.IdObraSocial.HasValue && client.IdObraSocial != clientToUpdate.IdObraSocial)
+            {
+                clientToUpdate.IdObraSocial = client.IdObraSocial;
+            }
+            _context.Clientes.Update(clientToUpdate);
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public async Task<List<ClientResponseDTO>> GetAllClients()
+        {
+            var clientes = await _context.Clientes
+                .Include(c => c.IdCiudadNavigation)
+                    .ThenInclude(l => l.IdLocalidadNavigation)
+                        .ThenInclude(p => p.IdProvinciaNavigation)
+                .Include(c => c.IdObraSocialNavigation)
+                .Select(c => new ClientResponseDTO
+                {
+                    IdClient= c.IdCliente,
+                    Name = c.Nombre,
+                    Lastname = c.Apellido,
+                    Email = c.CorreoElectronico,
+                    Street = c.Calle,
+                    Number= c.Altura,
+                    City = c.IdCiudadNavigation.Nombre,
+                    State = c.IdCiudadNavigation.IdLocalidadNavigation.IdProvinciaNavigation.Nombre,
+                    HealthPlan = c.IdObraSocialNavigation.Nombre,
+                }).ToListAsync();
+            return clientes;
+        }
+    }
+}
---
+++ Backend/JurassicPharm/Services/Clients/Implementations/ClientService.cs
@@ -0,0 +1,41 @@
+﻿using JurassicPharm.DTO.Clients;
+using JurassicPharm.Models;
+using JurassicPharm.Repositories.Clients.Implementations;
+using JurassicPharm.Repositories.Clients.Interfaces;
+using JurassicPharm.Services.Clients.Interfaces;
+
+namespace JurassicPharm.Services.Clients.Implementations
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+        public ClientService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+        public Task<bool> CreateClient(CreateClientDTO client)
+        {
+            return _clientRepository.CreateClient(client);
+        }
+
+        public Task<bool> DeleteClient(int idCliente)
+        {
+            return _clientRepository.DeleteClient(idCliente);
+        }
+
+        public Task<List<ClientResponseDTO>> GetAllClients()
+        {
+            return _clientRepository.GetAllClients();
+        }
+
+        public Task<Cliente> GetClientById(int idCliente)
+        {
+            return _clientRepository.GetClienteById(idCliente);
+        }
+
+        public Task<bool> UpdateClient(UpdateClientDTO client, int idCliente)
+        {
+            return _clientRepository.UpdateClient(client, idCliente);
+        }
+    }
+}
