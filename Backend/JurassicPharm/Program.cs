63c934a99bb567aed6d0b56dad56b7c44cdd694a
Federico Wuthrich <wuthrich09@gmail.com>
2024-11-07 00:47:33 -0300

    FW - BK - Configure Program.cs

--- Backend/JurassicPharm/Program.cs
+++ Backend/JurassicPharm/Program.cs
@@ -1,11 +1,19 @@
 using JurassicPharm.Models;
+using JurassicPharm.Repositories.Clients.Implementations;
+using JurassicPharm.Repositories.Clients.Interfaces;
 using JurassicPharm.Repositories.Invoices;
-using JurassicPharm.Services.Invoices;
 using JurassicPharm.Repositories.Personnel.Implementations;
 using JurassicPharm.Repositories.Personnel.Interfaces;
+using JurassicPharm.Repositories.Supplies.implementations;
+using JurassicPharm.Repositories.Supplies.Interfaces;
+using JurassicPharm.Services.Clients.Implementations;
+using JurassicPharm.Services.Clients.Interfaces;
+using JurassicPharm.Services.Invoices;
+using JurassicPharm.Services.JWT;
 using JurassicPharm.Services.Personnel.Implementations;
 using JurassicPharm.Services.Personnel.Interfaces;
-using JurassicPharm.Services.JWT;
+using JurassicPharm.Services.Supplies.Implementations;
+using JurassicPharm.Services.Supplies.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -15,7 +23,7 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 /**
- * Seccion de declaracion de CORS para frontend
+ * CORS Policy to allow the frontend to access the API
  * */
 builder.Services.AddCors(options =>
 {
@@ -27,32 +35,39 @@ builder.Services.AddCors(options =>
 
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
 //JWT Tokenization
 builder.Services.AddScoped<JwtService>();
 
+//Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //DB Contex
-
 builder.Services.AddDbContext<JurassicPharmContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //DI
+
+/*--Personnel--*/
 builder.Services.AddScoped<IPersonnelService, PersonnelService>();
 builder.Services.AddScoped<IPersonnelRepository, PersonnelRepository>();
+/*--Invoices--*/
 builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+/*--Supplies--*/
+builder.Services.AddScoped<ISuppliesService, SuppliesService>();
+builder.Services.AddScoped<IsuppliesRepository, SuppliesRepository>();
+/*--Clients--*/
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
 
 //Authorization
@@ -79,6 +94,7 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
     };
 });
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
